6a0ae03e0fe9b02b68708669a382a9bd293f9f98
krobingit <liorobin5@gmail.com>
2021-12-07 16:26:45 +0530

    Change

--- helper/helper.resetPassword.js
+++ helper/helper.resetPassword.js
@@ -28,17 +28,14 @@ export const helper = {
 
 
     let link = `https://password-reset-mern.netlify.app/resetPassword/${userDB._id}/${token}`;
-    try {
-      await sendMail(userDB.email, "Password Reset", `Hi ${userDB.username}!! Update your new password through this link
-  ${link}`);
-    }
-    catch (err)
-    {
-    res.send({"error in mail sending":err})
-    }
+
+    const emailresult=  await sendMail(userDB.email, "Password Reset", `${link}`);
+
+
+
     res.status(200).send({
       message:
-        "Reset link sent to mail"
+        "Reset link sent to mail",emailresult:emailresult
     });
 
 },
