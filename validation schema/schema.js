dbb35cb61453ef11ca94e0c69024b46ca3acff01
krobingit <liorobin5@gmail.com>
2021-12-09 10:43:39 +0530

    change

--- validation schema/schema.js
+++ validation schema/schema.js
@@ -3,8 +3,8 @@ import joi from 'joi';
 const registerSchema = joi.object({
  username: joi.string().alphanum().min(3).max(30).required(),
  email:joi.string().email().required(),
- password: joi.string().pattern(new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/)).required(),
- createdAt: joi.object().required()
+ password: joi.string().pattern(new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/)).required()
+
 })
 
 const loginSchema = joi.object({
